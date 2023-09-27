239720c80f3fec017708b586918c0468f33024c0
yuichirosogood <yuichirosogood@gmail.com>
2023-09-27 15:30:48 +0900

    移植終わり

--- afg.py
+++ afg.py
@@ -35,5 +35,6 @@ async def on_ready():
     await bot.load_extension("cogs.amanda.amanda")
     await bot.load_extension("cogs.message_register.message_register")
     await bot.load_extension("cogs.selfmention.selfmention")
+    await bot.load_extension("cogs.generatemap.generatemap")
 
 bot.run(TOKEN)
