0b67affc76fff563c2452ca2297efe0902346a95
yuichirosogood <yuichirosogood@gmail.com>
2024-02-08 01:27:06 +0900

    できたあああああああああ

--- cogs/u1quiz/u1quiz.py
+++ cogs/u1quiz/u1quiz.py
@@ -2,21 +2,32 @@ from discord.ext import commands
 import discord
 import random
 from cogs.u1quiz.questions import *
+
+class AnswerRecord():
+    def __init__(self,question,answer,iscorreft):
+        self.question = question
+        self.answer = answer
+        self.iscorrect = iscorreft
     
-class Result():
+class Score():
     def __init__(self):
         self.records = []
 
-    def recordscore(self,question,answer,iscorrect):
-        self.records.append((question,answer,iscorrect))
+    def addanswerrecord(self,answerrecord):
+        self.records.append(answerrecord)
 
-    def getfinalresult(self):
-        result = "# 【終了！】\n```md\n# 【RESULT】\n```"
-        for answer in self.records:
-            result += self.records
-            result += answer[0].question
+    def getfinalresultmsg(self):
+        score = 0
+        result = "# 【終了！】\n```md\n# 【RESULT】\n```\n"
+        for i, record in enumerate(self.records):
+            score = score+1 if record.iscorrect else score
+            result += ":white_check_mark: **正解！**\n" if record.iscorrect else ":x:  **不正解……**\n"
+            result += f"```md\n{i+1}. {record.question.question} → あなたの回答「{record.answer}」\n```\n"
+            result += f"> {record.question.reason}\n\n"
+        result += f"# あなたのスコア： {score} / {len(self.records)}\n# 称号は「{self.getmastery(score)}」です！"
+        return result
     
-    def getmastery(self):
+    def getmastery(self,score):
         mastery = [
             "ゆういちプロフェッショナル",
             "ゆういちマニア",
@@ -25,26 +36,31 @@ class Result():
             "冒涜 いちろう",
             "ゆう無知ろう"
             ]
-        
+        return mastery[len(mastery) - score]
+
 class Choice(discord.ui.Button):
-    def __init__(self,label,callback,correct=False):
+    def __init__(self,label,currentquestion,nextquestion,score,correct=False):
         super().__init__(label=label,style=discord.ButtonStyle.success)
         if correct:
             self.custom_id="correct"
-        self.cullbeckk = callback
+        self.currentquestion = currentquestion
+        self.score = score
+        self.nextquestion = nextquestion
 
     async def callback(self, interaction: discord.Interaction):
-        await interaction.response.send_message("")
-        await self.cullbeckk()
+        await interaction.response.defer()
+        #await interaction.response.send_message(content="：｝",delete_after=1)
+        self.score.addanswerrecord(AnswerRecord(self.currentquestion,self.label, self.custom_id=="correct"))
+        await self.nextquestion()
 
         
 class ChoicesView(discord.ui.View):
-    def __init__(self,callback,question):
+    def __init__(self,callback,question,score):
         super().__init__(timeout=180)
 
         for choice in random.sample(question.choices,len(question.choices)):
             #正解の選択肢ならcorrect=Trueにして送信
-            self.add_item(Choice(choice,callback,True) if choice == question.choices[0] else Choice(choice,callback))
+            self.add_item(Choice(choice,question,callback,score,True) if choice == question.choices[0] else Choice(choice,question,callback,score))
     
 
 class Quiz():
@@ -77,25 +93,33 @@ class Quiz():
 
         self.msg = None
 
-        self.result = Result()
+        self.score = Score()
     
     async def start(self):
         self.msg = await self.ctx.send(content=self.header)
         await self.next()
 
     async def next(self):
-
-        if self.current > self.qnum:
+                
+        if self.current >= self.qnum:
+            await self.finish()
             return
         
-        choicesview = ChoicesView(self.next, self.questions[self.current])
+        currentquestion = self.questions[self.current]
 
-        question_message = f"```md\n# 【第{self.current+1} / {self.qnum}問】\n{self.questions[self.current].question}\n```"
+        choicesview = ChoicesView(self.next, currentquestion, self.score)
+
+        question_message = f"```md\n# 【第 {self.current+1} / {self.qnum} 問】\n{currentquestion.question}\n```"
+
         content = self.header + question_message
 
         await self.msg.edit(content=content,view=choicesview)
 
         self.current += 1
+
+    async def finish(self):
+        await self.ctx.send(self.score.getfinalresultmsg())
+
 
 class U1quiz(commands.Cog):
     def __init__(self, bot):
