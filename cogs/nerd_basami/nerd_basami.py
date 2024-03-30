daf0e5ad033170ca620d9d8a3744e6a037f39631
yuichirosogood <yuichirosogood@gmail.com>
2024-03-30 12:18:04 +0900

    笑い

--- cogs/nerd_basami/nerd_basami.py
+++ cogs/nerd_basami/nerd_basami.py
@@ -6,15 +6,16 @@ class NerdBasami(commands.Cog):
         self.bot = bot
 
     @commands.command(aliases=["NerdBasami","オタク挟み","OtakuHasami","OtakuBasami","otabasami","オタ挟み","おたばさみ"])
-    async def NerdHasami(self, ctx):
+    async def NerdHasami(self, ctx, hasami="🤓"):
         cmd = ctx.message
         if cmd.reference == None:
             await cmd.channel.send("このコマンドは返信内で使用してね（右クリック → 返信）")
             return
         
         targetmsg = await cmd.channel.fetch_message(cmd.reference.message_id)
+        sendmsg = hasami + hasami.join(targetmsg.content) + hasami
 
-        await ctx.send("🤓" + "🤓".join(targetmsg.content) + "🤓")
+        await ctx.send(sendmsg[:2000])
 
 async def setup(bot):
     await bot.add_cog(NerdBasami(bot))
